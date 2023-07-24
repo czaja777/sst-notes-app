5e40dc4378a72b34492144480657de0b80080c8f
= <magdalenaanna.czajka@gmail.com>
2023-07-24 16:20:01 +0200

    "Adding a timeout"

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -25,6 +25,8 @@ export const main = handler(async (event) => {
   }
 
   // allocMem();
+  // Set a timeout
+  await new Promise((resolve) => setTimeout(resolve, 10000));
 
   // Return the retrieved item
   return result.Item;
